e092835a17b188d1fd9ca146858caa001adce7d5
lolux <LO@los-mac.cfl.rr.com>
2013-09-10 11:37:56 -0400

    renamed plugin to Clef deLux

---
+++ wpclef.php
@@ -0,0 +1,309 @@
+<?php
+/*
+Plugin Name: Clef deLux
+Plugin URI: https://github.com/lolux/wpclef-deLux
+Description: Clef lets you log in and register on your Wordpress site using only your phone — forget your usernames and passwords.
+Version: 1.6.2
+Author: LO
+Author URI: http://deDeo.org
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+ */
+
+/**
+
+Copyright (c) 2012 David Ross <dave@davidmichaelross.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+**/
+
+if ( ! defined('ABSPATH') ) exit('restricted access');
+
+if ( !session_id() ) {
+	session_start();
+}
+
+include dirname( __FILE__ )."/wpclef.admin.inc";
+
+
+if ( !isset( $_SESSION['WPClef_Messages'] ) ) {
+	$_SESSION['WPClef_Messages'] = array();
+}
+
+class WPClef {
+
+	const API_BASE = 'https://clef.io/api/v1/';
+
+	public static function setting( $name ) {
+		static $clef_settings = NULL;
+		if ( $clef_settings === NULL ) {
+			$clef_settings = get_option( 'wpclef' );
+		}
+		if ( isset( $clef_settings[$name] ) ) {
+			return $clef_settings[$name];
+		}
+
+		// Fall-through
+		return FALSE;
+
+	}
+
+	public static function init() {
+
+		if ( isset( $_REQUEST['clef_callback'] ) && isset( $_REQUEST['code'] ) ) {
+
+			// Authenticate
+
+			$args = array(
+				'code' => $_REQUEST['code'],
+				'app_id' => self::setting( 'clef_settings_app_id' ),
+				'app_secret' => self::setting( 'clef_settings_app_secret' ),
+			);
+
+			$response = wp_remote_post( self::API_BASE . 'authorize', array( 'method'=> 'POST', 'body' => $args, 'timeout' => 20 ) ); 
+
+			if ( is_wp_error($response)  ) {
+				$_SESSION['WPClef_Messages'][] = "Something went wrong: " . $response->get_error_message();
+				self::redirect_error();
+				return;
+			}
+
+			$body = json_decode( $response['body'] );
+
+			if ( !isset($body->success) || $body->success != 1 ) {
+				$_SESSION['WPClef_Messages'][] = 'Error retrieving Clef access token';
+				self::redirect_error();
+			}
+
+			$access_token = $body->access_token;
+			$_SESSION['wpclef_access_token'] = $access_token;
+
+			// Get info
+			$response = wp_remote_get( self::API_BASE . "info?access_token={$access_token}" );
+			if ( is_wp_error($response)  ) {
+				$_SESSION['WPClef_Messages'][] = "Something went wrong: " . $response->get_error_message();
+				self::redirect_error();
+				return;
+			}
+
+			$body = json_decode( $response['body'] );
+
+			if ( !isset($body->success) || $body->success != 1 ) {
+				$_SESSION['WPClef_Messages'][] = 'Error retrieving Clef user data.';
+				self::redirect_error();
+			}
+
+			$first_name = $body->info->first_name;
+			$last_name = $body->info->last_name;
+			$email = $body->info->email;
+			$clef_id = $body->info->id;
+
+			if (is_user_logged_in() && !get_user_meta(wp_get_current_user()->ID, "clef_id", true)) {
+				$existing_user = wp_get_current_user();
+				update_user_meta($existing_user->ID, 'clef_id', $clef_id);
+				$redirect = get_edit_profile_url($existing_user->ID) . "?updated=1";
+			} else {
+
+				$users = get_users(array('meta_key' => 'clef_id', 'meta_value' => $clef_id));
+				if ($users) $existing_user = $users[0];
+				else $existing_user =  WP_User::get_data_by( 'email', $email );
+
+				if ( !$existing_user ) {
+					$users_can_register = get_option('users_can_register', 0);
+					if(!$users_can_register) {
+						$_SESSION['WPClef_Messages'][] = "There's no user whose email address matches your phone's Clef account. You must either connect your Clef account on your WordPress profile page or use the same email for both WordPress and Clef.";
+						self::redirect_error();
+					}
+
+					// Register a new user
+					$userdata = new WP_User();
+					$userdata->first_name = $first_name;
+					$userdata->last_name = $last_name;
+					$userdata->user_email = $email;
+					$userdata->user_login = $email;
+					$password = wp_generate_password(16, FALSE);
+					$userdata->user_pass = $password;
+					$res = wp_insert_user($userdata);
+					if(is_wp_error($res)) {
+						$_SESSION['WPClef_Messages'][] = "An error occurred when creating your new account.";
+						self::redirect_error();
+					}
+					$existing_user = WP_User::get_data_by( 'email', $email );
+
+					update_user_meta($existing_user->ID, 'clef_id', $clef_id);
+
+				}
+
+				update_user_meta($existing_user->ID, 'clef_id', $clef_id);
+
+				$user = wp_set_current_user( $existing_user->ID, $existing_user->user_nicename );
+				wp_set_auth_cookie( $existing_user->ID );
+				do_action( 'wp_login', $existing_user->ID );
+
+				$redirect = admin_url();
+
+			}
+
+			// Log in the user
+			$_SESSION['logged_in_at'] = time();
+
+			header( "Location: " . $redirect );
+			exit();
+
+		}
+	}
+
+	public static function logout_handler() {
+
+		if(isset($_POST['logout_token'])) {
+
+			$args = array(
+				'logout_token' => $_REQUEST['logout_token'],
+				'app_id' => self::setting( 'clef_settings_app_id' ),
+				'app_secret' => self::setting( 'clef_settings_app_secret' ),
+			);
+
+			$response = wp_remote_post( self::API_BASE . 'logout', array( 'method' => 'POST',
+				'timeout' => 45, 'body' => $args ) ); 
+			$body = json_decode( $response['body'] );
+
+			if (isset($body->success) && $body->success == 1 && isset($body->clef_id)) {
+				$user = get_users(array('meta_key' => 'clef_id', 'meta_value' => $body->clef_id));
+				$user = $user[0];
+
+				// upon success, log user out
+				update_user_meta($user->ID, 'logged_out_at', time());
+			}
+		}
+	}
+
+	public static function login_form() {
+		$app_id = self::setting( 'clef_settings_app_id' );
+		$redirect_url = trailingslashit( home_url() ) . "?clef_callback=clef_callback&";
+		include dirname( __FILE__ )."/login_page.tpl.php";
+	}
+
+	public static function show_user_profile() {
+		$connected = !!get_user_meta(wp_get_current_user()->ID, "clef_id", true);
+		$app_id = self::setting( 'clef_settings_app_id' );
+		$redirect_url = trailingslashit( home_url() ) . "?clef_callback=clef_callback&connecting=true";
+		include dirname( __FILE__ )."/user_profile.tpl.php";
+	}
+
+	public static function edit_user_profile_update($user_id) {
+		if (isset($_POST['remove_clef']) && $_POST['remove_clef']) {
+			delete_user_meta($user_id, "clef_id");
+		}
+	}
+
+	public static function edit_profile_errors($errors) {
+		if ($_SESSION['WPClef_Messages']) {
+			$_SESSION['WPClef_Messages'] = array_unique( $_SESSION['WPClef_Messages'] );
+			echo '<div class="error">';
+			foreach ( $_SESSION['WPClef_Messages'] as $message ) {
+				echo '<p><strong>ERROR</strong>: '. $message . ' </p>';
+			}
+			echo '</div>';
+			$_SESSION['WPClef_Messages'] = array();
+		}
+	}
+
+	public static function login_message() {
+		$_SESSION['WPClef_Messages'] = array_unique( $_SESSION['WPClef_Messages'] );
+		foreach ( $_SESSION['WPClef_Messages'] as $message ) {
+			echo '<div class="message">' . $message . '</div>';
+		}
+		$_SESSION['WPClef_Messages'] = array();
+	}
+
+	public static function redirect_error() {
+		if (!is_user_logged_in()) {
+			header( 'Location: ' . wp_login_url() );
+		} else {
+			header( 'Location: ' . get_edit_profile_url(wp_get_current_user()->ID));
+		}
+		exit();
+	}
+
+	public static function logged_out_check($redirect=true) {
+		// if the user is logged into WP but logged out with Clef, sign them out of Wordpress
+		if (is_user_logged_in() && isset($_SESSION['logged_in_at']) && $_SESSION['logged_in_at'] < get_user_meta(wp_get_current_user()->ID, "logged_out_at", true)) {
+			wp_logout();
+			if ($redirect) {
+				self::redirect_error();
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function disable_passwords($user) {
+		if (self::setting("clef_password_settings_disable_passwords") == 1 && get_user_meta($user->ID, 'clef_id')) {
+			$_SESSION['WPClef_Messages'][] = "Error: logging in with a username and password has been disabled.";
+			header("Location: " . wp_login_url());
+			exit();
+		}
+
+		return $user;
+	}
+
+	public static function clear_logout_hook($user) {
+		if (isset($_SESSION['logged_in_at'])) {
+			unset($_SESSION['logged_in_at']);
+		}
+		return $user;
+	}
+
+	public static function hook_heartbeat($response, $data, $screen_id) {
+		$logged_out = self::logged_out_check(false);
+		if ($logged_out) {
+			$response['cleflogout'] = true;
+		}
+		return $response;
+	}
+	
+	/*** LO edit 1 of 2 */
+	public static function disable_login_form() {
+		// if disable passwords is set in plugin options, then hide the user/pass input form on login screen and show Clef login button only
+		if (self::setting("clef_password_settings_disable_passwords") == 1) {
+				if (is_user_logged_in()) {
+					header("Location: " . admin_url());
+					exit();
+				} else {
+					wp_enqueue_script('jquery');
+					login_header(__('Log In'), ''); ?>
+					<form name="loginform" id="loginform" action="" method="post">
+					<?php do_action('login_form'); ?>
+					</form>
+					<?php login_footer();	
+					exit();
+				}
+
+		}
+	}
+	/* end Lo edit 1 of 2 ***/
+}
+
+
+
+add_action( 'init', array( 'WPClef', 'init' ) );
+add_action( 'login_form', array( 'WPClef', 'login_form' ) );
+add_action( 'login_form_login', array( 'WPClef', 'disable_login_form' ) ); /***LO edit 2 of 2 ***/
+add_action( 'login_message', array( 'WPClef', 'login_message' ) );
+add_action('init', array('WPClef', 'logout_handler'));
+add_action('init', array('WPClef', 'logged_out_check'));
+add_action('show_user_profile', array('WPClef', 'show_user_profile'));
+add_action('edit_user_profile', array('WPClef', 'show_user_profile'));
+add_action('edit_user_profile_update', array('WPClef', 'edit_user_profile_update'));
+add_action('personal_options_update', array('WPClef', 'edit_user_profile_update'));
+add_action('admin_notices', array('WPClef', 'edit_profile_errors'));
+
+add_filter( 'heartbeat_received',  array("WPClef", "hook_heartbeat"), 10, 3);
+add_filter('wp_authenticate_user', array('WPClef', 'clear_logout_hook'));
+add_filter('wp_authenticate_user', array('WPClef', 'disable_passwords'));
